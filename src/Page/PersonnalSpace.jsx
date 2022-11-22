9b00ff71bc1b1fca2deefb2a0bdbe3a160e8f720
Cat-Cake <lucas.manot@gmail.com>
2022-11-22 18:00:12 +0100

    styling showing files

--- src/Page/PersonnalSpace.jsx
+++ src/Page/PersonnalSpace.jsx
@@ -64,7 +64,7 @@ const PersonnalSpace = () => {
                   <p>Tout cocher</p>
               </div>
               <button className="btn btn-full">PLus d'action</button>
-              <a href={`upload`}>Téléversement</a>
+              <a href={`upload`}><button className={"btn btn-full"}>Téléversement</button></a>
           </article>
           <section className={"show-data-list"}>
 
